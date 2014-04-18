e2dac06e6035da3b39a4a486b11fa99f41cf51e2
ApatiAti <tntiti@hotmail.com>
2014-04-18 20:58:25 +0200

    Geometry kész
    A Geometry-ben a tile-ok tárolása két dimenziós tömb lett .

--- Geometry.java
+++ Geometry.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 
 public class Geometry {
-	private Tile[] tiles;
+	private Tile[][] tiles;
+	private int size_x;
+	private int size_y;
 
 	/**
 	 * A Geometry osztály konstruktora.
@@ -14,8 +16,13 @@ public class Geometry {
 		tiles = new Tile[size];
 	}*/
 	
-	public void setMapSize(int size) {
-		tiles = new Tile[size];
+	/**
+	 * Létrehozza egy általunk kért nagyságú két dimmenziós térképet
+	 */
+	public void setMapSize(int sizex , int sizey) {
+		size_x=sizex;
+		size_y=sizey
+		tiles = new Tile[sizex][sizey];
 	}
 	
 	/**
@@ -33,19 +40,52 @@ public class Geometry {
 		// a targets lista az teszt target egz listaba agyazva
 		targets.add(tesztTarget);
 		return targets;	*/
-		return null;
+		
+		int center_pozition_x = -1;
+		int center_pozition_y = -1;
+		
+		
+		// A center tile pozíciójának megkeresése
+		// TODO  baj-e az hogy miután megtalálta a keresett Tile-t akkor még feleslegesn futt.
+		for(int x=0 ; x<size_x ; x++){
+			for(int y=0 ; y<size_y; y++){
+				if(tiles[x][y] == center){
+					center_pozition_x = x;
+					center_pozition_y = y;
+				}
+			}
+		}
+		
+		if(center_pozition_x == -1 || center_pozition_y == -1){
+			// TODO Ez azt jelentené hogy a center Tile nincs a térképen nem tudom hogy foglalkozni kéne ezzel
+		}
+		
+		ArrayList<Tile> result = new Arraylist();
+		
+		// megnézzük a center tile közelében lévő csempéket
+		for( int x=center_pozition_x-range     ; x<=center_pozition_x+range ; x++){
+			for( int y=center_pozition_x-range ; y<=center_pozition_x+range ; y++){ 
+				
+				// ha egy csempe a center csempétől vett távolságának kerekített értéke kisebb vagy egyenlő a hatótávolságnál akkor hatótávon belül van
+				if( range>=round(sqrt(x*x+y*y)) ){
+					result.add(tiles[x][y]);
+				}
+			}
+		}
+		
+		return result;
 	}
 	
 	/**
 	 * Visszaadja az összes csempét
 	 * @return Az összes csempe
 	 */
-	public Tile[] getTiles() { 
+	public Tile[][] getTiles() { 
 		return tiles;							//Visszaadja az összes csempét
 	}
 	
-	public void addTile(Tile tile, int index) {
-		tiles[index]=tile;
+	public void addTile(Tile tile, int index_x , int index_y) {
+		tiles[index_x][index_y]=tile;
 	}
 }
 
